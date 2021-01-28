4c267ebc25c426082fe3ca1ed85cbc5781a9c090
somoye123 <somoye.ayotunde@gmail.com>
2021-01-28 11:41:20 +0100

    add custom validation to company email

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,7 @@
 class Company < ApplicationRecord
   has_rich_text :description
+
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@getmainstreet.com\z/
+  validates :email, format: { with: VALID_EMAIL_REGEX,  message: "must end with @getmainstreet.com" }, allow_blank: true
+
 end
