3596048ea92e5464c6f84660e973f6e560d58510
JP Sio <hello@jpsio.me>
2021-01-27 18:35:24 -0800

    House keeping; ruby and js linting, prettier and asdf config

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
-require 'rails/test_help'
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
