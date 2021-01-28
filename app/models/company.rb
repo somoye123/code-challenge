5efc14f7b3af5c253111b78fc2d9cdebe51e6e3c
somoye123 <somoye.ayotunde@gmail.com>
2021-01-28 11:58:06 +0100

    add background_color method for adding color

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,4 +4,7 @@ class Company < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@getmainstreet.com\z/
   validates :email, format: { with: VALID_EMAIL_REGEX,  message: "must end with @getmainstreet.com" }, allow_blank: true
+  def background_color
+    self.color || DEFAULT_COLOR
+  end
 end
