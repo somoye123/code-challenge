40985ee57c706675c36281f5c223f9e5cb4c4d99
somoye123 <somoye.ayotunde@gmail.com>
2021-01-28 11:54:22 +0100

    migrate database to update companies in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_120421) do
+ActiveRecord::Schema.define(version: 2021_01_28_105220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_10_29_120421) do
     t.string "phone"
     t.string "email"
     t.string "services", default: [], array: true
+    t.string "color"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
